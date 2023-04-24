2f9ba211b6b5164b406af1ea710a186a122dae6f
Audun Larsen <larsen@xqus.com>
2023-04-24 21:16:28 +0200

    Fix tests

--- tests/BaseFilterTest.php
+++ tests/BaseFilterTest.php
@@ -1,32 +1,11 @@
 <?php
-namespace PhpWaf;
+namespace xqus\PhpWaf;
 
 use PHPUnit\Framework\TestCase;
 
 class BaseFilterTest extends TestCase
 {
-    public function testAttributes()
-    {
-        $attributes = array(
-            'payloads',
-            'payloads_file',
-        );
 
-        foreach ($attributes as $attribute)
-        {
-            $this->assertClassHasAttribute($attribute, BaseFilter::class);
-        }
-    }
-
-    public function testFilterAttributes()
-    {
-        $firewall = new Firewall();
-
-        foreach (array_keys($firewall->getFilters()) as $filter)
-        {
-            $this->assertClassHasAttribute('payloads_file', "PhpWaf\\Filter\\$filter");
-        }
-    }
 
     public function testFilterMethods()
     {
@@ -34,7 +13,7 @@ class BaseFilterTest extends TestCase
 
         foreach (array_keys($firewall->getFilters()) as $filter)
         {
-            $className = "PhpWaf\\Filter\\$filter";
+            $className = "xqus\\PhpWaf\\Filter\\$filter";
             $object = new $className;
             $this->assertTrue(method_exists($object, "safe"));
         }
